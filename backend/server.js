3e4bc24d55ea74226bb96722a4b931027feba0d7
Noel <noeltesdev@gmail.com>
2025-06-25 15:01:54 -0600

    modified

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 // console.log('Starting server...');
 import dotenv from 'dotenv';
-dotenv.config();
+// dotenv.config();
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
