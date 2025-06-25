4c76bc3b49e756f36b529ac0e22306c49061705b
Noel <noeltesdev@gmail.com>
2025-06-25 02:20:55 -0600

    modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import typography from '@tailwindcss/typography';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), typography],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000', // proxy /api requests to backend
-    },
-  },
+  plugins: [react(), tailwindcss()],
+ 
 });
