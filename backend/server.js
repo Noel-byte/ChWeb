3f00f4dc851da9bac107ccccced6b5f3cde69c64
Noel <noeltesdev@gmail.com>
2025-06-25 13:24:42 -0600

    modified

--- backend/server.js
+++ backend/server.js
@@ -23,13 +23,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get(
-  '*',
-  (req,
-  (res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-  })
-);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.use('/api/stripe', webhookRoutes);
 // const allowedOrigins = [
