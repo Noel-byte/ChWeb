1708938e24c7e0d70dd7a38ba28d8d6ba27b8b90
Noel <noeltesdev@gmail.com>
2025-06-25 16:33:33 -0600

    modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,4 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    build: {
-    outDir: 'dist' // this should be 'dist' by default
-  }
 });
