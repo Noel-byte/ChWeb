dfb99d3ea9cd7991fe62d320dc359d23e9575f5a
Noel <noeltesdev@gmail.com>
2025-06-26 11:30:30 -0600

    modified

--- backend/build-frontend.sh
+++ backend/build-frontend.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "📦 Building frontend..."
+echo "📦 Installing frontend dependencies..."
 cd ../frontend || exit 1
 npm install
+
+echo "📦 Building frontend..."
 npm run build || exit 1
 
 echo "📂 Copying dist to backend..."
 rm -rf ../backend/dist
 cp -r dist ../backend/ || exit 1
 
-echo "✅ Frontend build complete."
